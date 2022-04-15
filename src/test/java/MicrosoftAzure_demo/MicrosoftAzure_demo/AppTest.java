5c11bf63666c0241ba81bc5caed1b58997696f9f
kiran <kiran.ghorpade@hcl.com>
2022-04-15 13:46:44 +0530

    added two prints

--- src/test/java/MicrosoftAzure_demo/MicrosoftAzure_demo/AppTest.java
+++ src/test/java/MicrosoftAzure_demo/MicrosoftAzure_demo/AppTest.java
@@ -20,12 +20,13 @@ public class AppTest
 	public void test1(String browser) throws InterruptedException
 	{
 		if(browser.equals("chrome")){
-		
+		System.out.println("started chrome browser");
 		WebDriverManager.chromedriver().setup();
 		 driver=new ChromeDriver();
 		 driver.get("https://www.google.co.in/");
 		 Thread.sleep(2000);
 		 driver.close();
+			System.out.println("ended chrome browser");
 		}
 		else if(browser.equals("firefox")){
 			WebDriverManager.firefoxdriver().setup();
